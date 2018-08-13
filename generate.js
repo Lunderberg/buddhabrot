9c14ed133fb56ece4a7086f93797c0edb86763b6
Eldritch Cheese <eldritch.cheese@gmail.com>
2018-08-13 09:29:35 -0700

    Two-stage rendering, with density plot of the diverged points.

--- generate.js
+++ generate.js
@@ -91,10 +91,30 @@ var Buddhabrot = function(canvas, n_points_x, n_points_y) {
     this.n_points_y = n_points_y;
 
     this.canvas = canvas;
-    this.reset();
+
 
     this.stop_next = false;
     this.running = false;
+
+    this.display_iteration_num = null;
+    this.display_generation_stage = null;
+
+    // Limits of the view window.
+    // Not necessarily those of the iterated points.
+    var xmin = -2;
+    var xmax = +2;
+    var ymin = -2;
+    var ymax = +2;
+    var width = this.canvas.width;
+    var height = this.canvas.height;
+    this.hist_mandelbrot = new Histogram(width, xmin, xmax,
+                                         height, ymin, ymax);
+    this.hist_all = new Histogram(width, xmin, xmax,
+                                  height, ymin, ymax);
+    this.hist_diverged = new Histogram(width, xmin, xmax,
+                                       height, ymin, ymax);
+
+    this.reset();
 };
 
 Buddhabrot.prototype.start = function() {
@@ -124,6 +144,22 @@ Buddhabrot.prototype.reset = function() {
                     new MandelbrotPoint(
                         this.xmin + (this.xmax-this.xmin)*(i/this.n_points_x),
                         this.ymin + (this.ymax-this.ymin)*(j/this.n_points_y)))).flatten();
+
+    this.hist_mandelbrot.clear();
+    this.hist_all.clear();
+    this.hist_diverged.clear();
+    this.current_stage = 'Preliminary';
+};
+
+Buddhabrot.prototype.enter_final_stage = function() {
+    this.num_iterations = 0;
+    this.current_stage = 'Final';
+
+    this.mandelbrot = this.mandelbrot
+        .filter(point => point.diverged)
+        .map( point => new MandelbrotPoint(
+            point.orig_real, point.orig_imag
+        ));
 };
 
 Buddhabrot.prototype.callback = function() {
@@ -131,13 +167,26 @@ Buddhabrot.prototype.callback = function() {
     var update_every = document.getElementById('redraw-every').value;
     var delay = document.getElementById('delay').value;
 
-    if(this.stop_next || this.num_iterations > max_iter) {
+    if(this.stop_next ||
+       (this.num_iterations > max_iter && this.current_stage === 'Final')) {
         this.running = false;
         this.stop_next = false;
         return;
     }
 
+    if(this.num_iterations > max_iter && this.current_stage === 'Preliminary') {
+        this.enter_final_stage();
+    }
+
     this.iterate();
+
+    if(this.current_stage === 'Preliminary') {
+        this.update_histograms_preliminary();
+    } else if(this.current_stage === 'Final') {
+        this.update_histograms_final();
+    }
+
+
     if(this.iter_since_redraw > update_every) {
         this.draw();
     }
@@ -151,51 +200,63 @@ Buddhabrot.prototype.iterate = function() {
     );
     this.iter_since_redraw++;
     this.num_iterations++;
+
+    if(this.display_iteration_num !== null) {
+        this.display_iteration_num.innerHTML = this.num_iterations;
+    }
+
+};
+
+Buddhabrot.prototype.update_histograms_preliminary = function() {
+    this.hist_mandelbrot.clear();
+    this.mandelbrot
+        .filter(point => !point.diverged)
+        .forEach((point) => {
+            this.hist_mandelbrot.fill(point.orig_real, point.orig_imag);
+            this.hist_all.fill(point.real, point.imag);
+        });
+};
+
+Buddhabrot.prototype.update_histograms_final = function() {
+    this.mandelbrot
+        .filter(point => !point.diverged)
+        .forEach((point) => {
+            this.hist_diverged.fill(point.real, point.imag);
+        });
 };
 
 Buddhabrot.prototype.draw = function() {
-    // Limits of the view window.
-    // Not necessarily those of the iterated points.
-    var xmin = -2;
-    var xmax = +2;
-    var ymin = -2;
-    var ymax = +2;
-
-    width = this.canvas.width;
-    height = this.canvas.height;
-
     context = this.canvas.getContext('2d');
 
-
-    var hist = new Histogram(width, xmin, xmax,
-                             height, ymin, ymax);
-    hist.clear();
-    this.mandelbrot
-        .filter(point => !point.diverged)
-        .forEach(function(point) {
-            hist.fill(point.orig_real, point.orig_imag);
-        });
-    var img_data = context.createImageData(width, height);
-    hist.normalized().forEach((val,i) => {
+    var img_data = context.createImageData(this.canvas.width, this.canvas.height);
+    this.hist_mandelbrot.normalized().forEach((val,i) => {
         img_data.data[4*i+0] = 255*(1-val);
         img_data.data[4*i+1] = 255*(1-val);
         img_data.data[4*i+2] = 255*(1-val);
         img_data.data[4*i+3] = 255;
     });
-    context.putImageData(img_data, 0, 0);
+    createImageBitmap(img_data).then(function(bitmap) {
+        context.drawImage(bitmap, 0, 0);
+    });
 
 
-    hist.clear();
-    this.mandelbrot
-        .filter(point => !point.diverged)
-        .forEach(function(point) {
-            hist.fill(point.real, point.imag);
-        });
-    var img_data = context.createImageData(width, height);
-    hist.normalized().forEach((val,i) => {
+    var img_data = context.createImageData(this.canvas.width, this.canvas.height);
+    this.hist_all.normalized().forEach((val,i) => {
         img_data.data[4*i+0] = 255;
         img_data.data[4*i+1] = 0;
         img_data.data[4*i+2] = 0;
+        img_data.data[4*i+3] = 255*val;
+    });
+    createImageBitmap(img_data).then(function(bitmap) {
+        context.drawImage(bitmap, 0, 0);
+    });
+
+
+    var img_data = context.createImageData(this.canvas.width, this.canvas.height);
+    this.hist_diverged.normalized().forEach((val,i) => {
+        img_data.data[4*i+0] = 0;
+        img_data.data[4*i+1] = 0;
+        img_data.data[4*i+2] = 255;
         img_data.data[4*i+3] = 255*val;
     });
     createImageBitmap(img_data).then(function(bitmap) {
@@ -208,6 +269,8 @@ Buddhabrot.prototype.draw = function() {
 
 var canvas = document.getElementById('buddhabrot-canvas');
 var buddha = new Buddhabrot(canvas, 4*canvas.width, 4*canvas.height);
+buddha.display_iteration_num = document.getElementById('iteration-num');
+buddha.display_generation_stage = document.getElementById('generation-stage');
 buddha.start();
 
 
